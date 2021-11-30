756295e9b01264eae479f2d742b2d10ce01d0fa0
HemalathaMuddada <hemalathamuddada2@gmail.com>
2021-11-30 10:59:43 +0530

    commit

--- StudentManagementSystem/src/main/java/com/project/sms/mail/UserCreatedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.project.sms.mail;
-
-import org.springframework.context.ApplicationEvent;
-
-public class UserCreatedEvent extends ApplicationEvent {
-    public UserCreatedEvent(Object source) {
-        super(source);
-    }
-}
